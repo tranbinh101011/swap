2329654356e9b27a1118f540b9a51a230b3b42b2
chef-ryan <ryan@pancakeswap.com>
2025-02-17 14:37:49 +0800

    fix: 🐛 fix apr issues (#11213)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR enhances the type safety and robustness of the `sum` function in
    the `sumApr.ts` file by explicitly defining the accumulator's type and
    adding checks for finite numbers.
    
    ### Detailed summary
    - Changed the type of the accumulator in `reduce` to `<number>`.
    - Added a check for `Number.isFinite(x)` and `Number.isNaN(x)` to ensure
    valid numeric values.
    - Simplified the return statement to directly return `acc + x`.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/universalFarms/utils/sumApr.ts
+++ apps/web/src/views/universalFarms/utils/sumApr.ts
@@ -1,9 +1,14 @@
 export const sumApr = (...aprs: Array<number | `${number}` | undefined>): number => {
-  const sum = aprs.reduce((acc, apr) => {
+  const sum = aprs.reduce<number>((acc, apr) => {
     if (typeof apr === 'undefined') {
       return acc ?? 0
     }
-    return Number(acc ?? 0) + Number(apr ?? 0)
+    const x = Number(apr ?? 0)
+    if (!Number.isFinite(x) || Number.isNaN(x)) {
+      return acc
+    }
+
+    return acc + x
   }, 0)
   return Number(sum) ? Number(sum) : 0
 }
