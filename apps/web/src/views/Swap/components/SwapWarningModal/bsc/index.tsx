d5328bd432aed9d15dfb7e417c2e77223d91f404
Chef Jerry <144641937+ChefJerry@users.noreply.github.com>
2025-03-22 00:59:07 +0800

    chore(PAN-4598): remove pnp warning (#11377)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR removes references to the `PNPWarning` component and associated
    warnings related to the `PNP` token from the BSC warning list and
    translation files, indicating a decision to no longer display these
    warnings.
    
    ### Detailed summary
    - Deleted the import of `PNPWarning` from
    `apps/web/src/views/Swap/components/SwapWarningModal/bsc/index.tsx`.
    - Removed the `PNPWarning` entry from the `BSC_WARNING_LIST` object.
    - Removed the translation key for "Caution - PNP Token" from
    `packages/localization/src/config/translations.json`.
    - Removed the warning message related to the `PNP` token from the
    translations file.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/Swap/components/SwapWarningModal/bsc/index.tsx
+++ apps/web/src/views/Swap/components/SwapWarningModal/bsc/index.tsx
@@ -10,7 +10,6 @@ import ItamWarning from './ItamWarning'
 import LUSDWarning from './LUSDWarning'
 import METISWarning from './METISWarning'
 import NFPWarning from './NFPWarning'
-import PNPWarning from './PNPWarning'
 import RugPullWarning from './RugPullWarning'
 import SafemoonWarning from './SafemoonWarning'
 import XCADWarning from './XCADWarning'
@@ -70,10 +69,6 @@ const BSC_WARNING_LIST = {
   [nfp.address]: {
     symbol: nfp.symbol,
     component: <NFPWarning />,
-  },
-  [pnp.address]: {
-    symbol: pnp.symbol,
-    component: <PNPWarning />,
   },
 }
 
