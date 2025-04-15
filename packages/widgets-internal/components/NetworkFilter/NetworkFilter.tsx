c1adbd58f0b70e115da1618064f625a6df4cf0f9
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-04-15 17:07:47 +0800

    chore: [PAN4549] update the chain selection component UX (#11500)

--- packages/widgets-internal/components/NetworkFilter/NetworkFilter.tsx
+++ packages/widgets-internal/components/NetworkFilter/NetworkFilter.tsx
@@ -1,4 +1,5 @@
-import { MultiSelect, IMultiSelectChangeEvent, IMultiSelectProps } from "@pancakeswap/uikit";
+import { Trans } from "@pancakeswap/localization";
+import { Button, IMultiSelectChangeEvent, IMultiSelectProps, MultiSelect } from "@pancakeswap/uikit";
 import { useCallback, useState } from "react";
 import styled from "styled-components";
 
@@ -43,6 +44,40 @@ const Container = styled.div<{ $isShow: boolean }>`
  `}
 `;
 
+const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const StyledButton = styled(Button)`
+  position: absolute;
+  right: 50px;
+
+  height: 27px;
+  background-color: ${({ theme }) => theme.colors.input};
+  color: ${({ theme }) => theme.colors.text};
+  opacity: 0;
+  transition: opacity 0.3s ease-in;
+`;
+
+const StyledContainer = styled(Container)`
+  .p-multiselect-item {
+    padding: 8px 16px;
+    transition: background-color 0.2s ease;
+    cursor: pointer;
+    position: relative;
+    &:hover {
+      background-color: ${({ theme }) =>
+        theme.isDark ? "#4B3B5F" : "#E8E2EE"}; // secondary20 & wait for v4 to merge to use it
+      ${StyledButton} {
+        opacity: 1;
+      }
+    }
+  }
+`;
+
 export const NetworkFilter: React.FC<INetworkProps> = ({ data, value, onChange }: INetworkProps) => {
   const [isShow, setIsShow] = useState(false);
 
@@ -55,8 +90,46 @@ export const NetworkFilter: React.FC<INetworkProps> = ({ data, value, onChange }
     [onChange, data]
   );
 
+  const handleOnlyClick = useCallback(
+    (networkValue: number, e: React.MouseEvent) => {
+      e.stopPropagation();
+      onChange([networkValue], {
+        value: [networkValue],
+        originalEvent: e,
+        stopPropagation: e.stopPropagation,
+        preventDefault: e.preventDefault,
+      });
+    },
+    [onChange]
+  );
+
+  const customItemTemplate = useCallback(
+    (option: { label: string; value: number; icon?: React.ReactNode | string }) => {
+      return (
+        <ItemContainer>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {option.icon && (
+              <span style={{ marginRight: "8px" }}>
+                {typeof option.icon === "string" ? (
+                  <img src={option.icon} alt={option.label} width="24" />
+                ) : (
+                  option.icon
+                )}
+              </span>
+            )}
+            <span>{option.label}</span>
+          </div>
+          <StyledButton scale="xs" onClick={(e: React.MouseEvent) => handleOnlyClick(option.value, e)}>
+            <Trans>Only</Trans>
+          </StyledButton>
+        </ItemContainer>
+      );
+    },
+    [handleOnlyClick]
+  );
+
   return (
-    <Container $isShow={isShow}>
+    <StyledContainer $isShow={isShow}>
       <MultiSelect
         style={{
           backgroundColor: "var(--colors-input)",
@@ -72,7 +145,8 @@ export const NetworkFilter: React.FC<INetworkProps> = ({ data, value, onChange }
         onShow={() => setIsShow(true)}
         onHide={() => setIsShow(false)}
         onChange={handleSelectChange}
+        itemTemplate={customItemTemplate}
       />
-    </Container>
+    </StyledContainer>
   );
 };
