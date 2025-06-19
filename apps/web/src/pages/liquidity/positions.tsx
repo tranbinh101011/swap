627847b5d881f774acafa5b3034f7b69be766b58
chef-ryan <ryan@pancakeswap.com>
2025-06-19 21:46:09 +0800

    fix: liquidity position list chain selection (#11826)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR refactors the export of the `UniversalFarmsPage` component in
    `positions.tsx` to include type support for Next.js pages and adds a
    property to the page component.
    
    ### Detailed summary
    - Added import for `NextPageWithLayout` from `utils/page.types`.
    - Changed the export of `UniversalFarmsPage` to a constant `Page`.
    - Cast `Page` as `NextPageWithLayout`.
    - Added a property `chains` to `Page` with the value `SUPPORT_FARMS`.
    - Exported `Page` as the default export.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/pages/liquidity/positions.tsx
+++ apps/web/src/pages/liquidity/positions.tsx
@@ -1,6 +1,11 @@
+import { SUPPORT_FARMS } from 'config/constants/supportChains'
 import dynamic from 'next/dynamic'
+import { NextPageWithLayout } from 'utils/page.types'
 import UniversalFarmsPage from './pools'
 
-export default dynamic(() => Promise.resolve(UniversalFarmsPage), {
+const Page = dynamic(() => Promise.resolve(UniversalFarmsPage), {
   ssr: false,
-})
+}) as NextPageWithLayout
+
+Page.chains = SUPPORT_FARMS
+export default Page
