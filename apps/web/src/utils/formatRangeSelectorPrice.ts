e7f25a30bb621f34f550774cd5767c02d5f3d279
memoyil <2213635+memoyil@users.noreply.github.com>
2025-06-25 04:49:31 +0200

    fix: Increase decimals in range selector (#11811)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR updates the `formatRangeSelectorPrice.ts` utility function to
    change the precision of the price formatting based on its value.
    
    ### Detailed summary
    - Modified the return statement in the function to use
    `price.toSignificant(6)` instead of `price.toFixed(2)` when `price` is
    greater than 1.
    - Retained `price.toSignificant(9)` for values less than or equal to 1.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/utils/formatRangeSelectorPrice.ts
+++ apps/web/src/utils/formatRangeSelectorPrice.ts
@@ -3,5 +3,5 @@ import { Currency, Price } from '@pancakeswap/swap-sdk-core'
 export const formatRangeSelectorPrice = (price: Price<Currency, Currency> | undefined) => {
   if (!price) return ''
 
-  return price.greaterThan(1) ? price.toFixed(2) : price.toSignificant(9)
+  return price.greaterThan(1) ? price.toSignificant(6) : price.toSignificant(9)
 }
