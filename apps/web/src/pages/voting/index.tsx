f39a3f697176789598a6b7da67854de92398514e
chef-ryan <ryan@pancakeswap.com>
2025-07-21 11:42:29 +0800

    fix: Fix/proposal (#11953)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR focuses on updating the page components in the voting section of
    the application to utilize dynamic imports with server-side rendering
    disabled, improving performance and loading behavior.
    
    ### Detailed summary
    - Replaced static exports with dynamic imports for `Voting`,
    `CreateProposal`, and `ProposalView` components.
    - Changed the component name from `ProposalPage` to `ProposalView` in
    `apps/web/src/pages/voting/proposal/[id].tsx`.
    - Removed unused `getStaticPaths` and `getStaticProps` functions.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/pages/voting/index.tsx
+++ apps/web/src/pages/voting/index.tsx
@@ -1,3 +1,7 @@
+import dynamic from 'next/dynamic'
+import { NextPageWithLayout } from 'utils/page.types'
 import Voting from '../../views/Voting'
 
-export default Voting
+const Page = dynamic(() => Promise.resolve(Voting), { ssr: false }) as NextPageWithLayout
+
+export default Page
--- apps/web/src/pages/voting/proposal/create.tsx
+++ apps/web/src/pages/voting/proposal/create.tsx
@@ -1,3 +1,7 @@
+import dynamic from 'next/dynamic'
+import { NextPageWithLayout } from 'utils/page.types'
 import CreateProposal from '../../../views/Voting/CreateProposal'
 
-export default CreateProposal
+const Page = dynamic(() => Promise.resolve(CreateProposal), { ssr: false }) as NextPageWithLayout
+
+export default Page
